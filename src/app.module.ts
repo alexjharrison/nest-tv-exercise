892497ad9e7fd12217c319a672c2d8a54779e398
Alex Harrison <alexjharrison@gmail.com>
2024-05-06 17:32:16 -0400

    add resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TvShowsModule } from './tv-shows/tv-shows.module';
+import { ExercisesModule } from './exercises/exercises.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [TvShowsModule, ExercisesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
